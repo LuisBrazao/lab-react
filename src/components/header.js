2ee00ba730ade3791525857ff1b51b46e421b5e6
Luis Brazao <luis.ms.brazao@gmail.com>
2020-11-23 17:29:42 +0000

    just missing some style

---
+++ src/components/header.js
@@ -0,0 +1,14 @@
+function Header(){
+    return (
+        <div className="row" id="header">
+            <div className="col-4" id="text">
+                <h1>Say hello to ReactJS</h1>
+                <p>You will learn how to use the most popular frontend library, and become a super Ninja developer</p>
+                <button className="btn btn-primary my-btn">Awesome!</button>
+            </div>
+            <div className="col-6"></div>
+        </div>
+    )
+}
+
+export default Header
